3c9ce786ae1050d6b40fddb5f6e9c2c6dc473f85
midsorbet <l.khadka@outlook.com>
2025-12-13 21:48:23 -0800

    fix age on nixos

--- modules/nixos/secrets.nix
+++ modules/nixos/secrets.nix
@@ -3,27 +3,17 @@
 let user = "me"; in
 {
   age.identityPaths = [
-    "/home/${user}/.ssh/id_ed25519"
+    "/etc/ssh/ssh_host_ed25519_key"
   ];
 
-  # Your secrets go here
-  #
-  # Note: the installWithSecrets command you ran to boostrap the machine actually copies over
-  #       a Github key pair. However, if you want to store the keypair in your nix-secrets repo
-  #       instead, you can reference the age files and specify the symlink path here. Then add your
-  #       public key in shared/files.nix.
-  #
-  #       If you change the key name, you'll need to update the SSH configuration in shared/home-manager.nix
-  #       so Github reads it correctly.
-
-  #
-  # age.secrets."github-ssh-key" = {
-  #   symlink = false;
-  #   path = "/home/${user}/.ssh/id_github";
-  #   file =  "${secrets}/github-ssh-key.age";
-  #   mode = "600";
-  #   owner = "${user}";
-  #   group = "wheel";
-  # };
+  
+  age.secrets."github-ssh-key" = {
+    symlink = false;
+    path = "/home/${user}/.ssh/id_github";
+    file =  "${secrets}/github-ssh-key.age";
+    mode = "600";
+    owner = "${user}";
+    group = "wheel";
+  };
 
 }
