1ae287d853cf9e19ca0d38e944b1da7f99e8c619
midsorbet <6295956+midsorbet@users.noreply.github.com>
2026-04-24 15:34:18 -0700

    Inline zmx package locals

--- packages/zmx/default.nix
+++ packages/zmx/default.nix
@@ -1,7 +1,6 @@
 {inputs}: final: prev: let
   system = prev.stdenvNoCC.hostPlatform.system;
   version = "0.5.0";
-  zmxPlatforms = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
   zmxMeta = {
     description = "Session persistence for terminal processes";
     longDescription = ''
@@ -16,7 +15,7 @@
     downloadPage = "https://zmx.sh/#install";
     license = prev.lib.licenses.mit;
     mainProgram = "zmx";
-    platforms = zmxPlatforms;
+    platforms = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
   };
   tarballs = {
     "aarch64-darwin" = {
@@ -24,43 +23,48 @@
       hash = "sha256-O5N58P8M8Qf3+HBI0sRfb76r7ViNZ2rYasIYvtko0Qc=";
     };
   };
-  zmx-tarball = prev.stdenvNoCC.mkDerivation {
-    pname = "zmx";
-    inherit version;
-
-    src = prev.fetchurl tarballs.${system};
-
-    sourceRoot = ".";
-
-    unpackPhase = ''
-      tar -xzf "$src"
-    '';
-
-    installPhase = ''
-      install -Dm755 zmx "$out/bin/zmx"
-    '';
-
-    meta = zmxMeta;
-  };
   zmx =
     if builtins.hasAttr system tarballs
-    then zmx-tarball
+    then
+      prev.stdenvNoCC.mkDerivation {
+        pname = "zmx";
+        inherit version;
+
+        src = prev.fetchurl tarballs.${system};
+
+        sourceRoot = ".";
+
+        unpackPhase = ''
+          tar -xzf "$src"
+        '';
+
+        installPhase = ''
+          install -Dm755 zmx "$out/bin/zmx"
+        '';
+
+        meta = zmxMeta;
+      }
     else
       inputs.zmx.packages.${system}.zmx.overrideAttrs (old: {
         meta = (old.meta or {}) // zmxMeta;
       });
-  zmx-select-script = (prev.writeScriptBin "zmx-select" (builtins.readFile ./zmx-select.sh)).overrideAttrs (old: {
-    buildCommand = ''
-      ${old.buildCommand}
-      patchShebangs $out
-    '';
-  });
 in {
   inherit zmx;
 
   zmx-select = prev.symlinkJoin rec {
     name = "zmx-select";
-    paths = [zmx-select-script zmx prev.fzf prev.gawk prev.gnused];
+    paths = [
+      ((prev.writeScriptBin "zmx-select" (builtins.readFile ./zmx-select.sh)).overrideAttrs (old: {
+        buildCommand = ''
+          ${old.buildCommand}
+          patchShebangs $out
+        '';
+      }))
+      zmx
+      prev.fzf
+      prev.gawk
+      prev.gnused
+    ];
     nativeBuildInputs = [prev.makeWrapper];
     postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
   };
