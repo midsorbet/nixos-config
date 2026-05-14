4c7b7162bfc294af194c53ab526446a418afbf5a
midsorbet <6295956+midsorbet@users.noreply.github.com>
2026-05-14 10:58:48 -0700

    chore(mini-darwin): bump kittylitter to 0.3.1

--- hosts/mini-darwin/kittylitter.nix
+++ hosts/mini-darwin/kittylitter.nix
@@ -6,14 +6,14 @@
 }: let
   user = config.system.primaryUser;
   home = config.system.primaryUserHome;
-  version = "0.2.9";
+  version = "0.3.1";
   package = pkgs.stdenvNoCC.mkDerivation {
     pname = "kittylitter";
     inherit version;
 
     src = pkgs.fetchurl {
       url = "https://github.com/dnakov/litter/releases/download/v${version}/kittylitter-aarch64-apple-darwin.tar.xz";
-      hash = "sha256-LzxPdcgdD410/dZN5gkgqek/VL+++SW9rsG6Ly0NnPU=";
+      hash = "sha256-IcVec9nWlIohnqxQO/wJ1fzCdBWm7ku9yJW/ziUr89k=";
     };
 
     sourceRoot = "kittylitter-aarch64-apple-darwin";
