658be9c91901a02863a93753050869394eea60bd
midsorbet <6295956+midsorbet@users.noreply.github.com>
2026-07-03 14:40:09 -0700

    chore(upgrades): refresh flake inputs and awscli2

--- packages/awscli2.nix
+++ packages/awscli2.nix
@@ -1,19 +1,19 @@
 {pkgs}:
 pkgs.awscli2.overridePythonAttrs (old: let
   awscrt = pkgs.python3.pkgs.awscrt.overridePythonAttrs (oldAwscrt: rec {
-    version = "0.32.2";
+    version = "0.35.0";
     src = oldAwscrt.src.override {
       inherit version;
-      hash = "sha256-pPSIBeimYjeSPwO3tpLSE5lM/0LR/wgSXR1gx0/K+HI=";
+      hash = "sha256-dhrg3aF/2d+v9Luyo3bijkTf133GQQt7xAgpeh/VYA4=";
     };
   });
 in rec {
-  version = "2.35.14";
+  version = "2.35.15";
   src = pkgs.fetchFromGitHub {
     owner = "aws";
     repo = "aws-cli";
     tag = version;
-    hash = "sha256-IBBd4NnbdLs0F094QA1bE7O8Oux93lAdmGduePEpb/8=";
+    hash = "sha256-uQEf1X5FW3mjFPKI5aEqlqcIh8N/PIiH7KIUVOvqxzQ=";
   };
 
   dependencies = [awscrt] ++ pkgs.lib.tail old.dependencies;
