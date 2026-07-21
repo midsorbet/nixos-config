69396afd23d84842c855ea671fe48cdf8d68980c
midsorbet <6295956+midsorbet@users.noreply.github.com>
2026-07-20 20:22:38 -0700

    chore(devices): retire Bokoblin access

--- hosts/baymax/default.nix
+++ hosts/baymax/default.nix
@@ -16,7 +16,6 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFs1Ljh6faseFzEG9B0jufOsmc8wMIDxMwiROfp9u3zC"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvCa1xa2EJLNl4lTFtBSPDWpi0uiuE34kpCxkfDYz8r mini-darwin nix builder for baymax"
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBO/2RV9P8Z2/CMbghca654D4sbQ5zbUc7tOJ+x2tcUWILJV3bXeAPI3O+Y65yDU7CojTYje22WBOAWqysmv4LTs= me@moblin"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxlO3eFi2yvrVH9bSsEyMMvzjRGWIoImlK+ixy2FIcy bokoblin"
     ];
   };
 in {
