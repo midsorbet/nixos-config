04f4186fa09377bd3e9692148ed1685eab41c712
midsorbet <6295956+midsorbet@users.noreply.github.com>
2026-08-24 22:31:28 -0700

    paperless: encode comma-list settings correctly

--- hosts/baymax/default.nix
+++ hosts/baymax/default.nix
@@ -394,10 +394,10 @@ in {
       configureTika = true;
       settings = {
         PAPERLESS_URL = "https://paperless.${domain}";
-        PAPERLESS_ALLOWED_HOSTS = ["paperless.${domain}"];
-        PAPERLESS_CSRF_TRUSTED_ORIGINS = ["https://paperless.${domain}"];
-        PAPERLESS_CORS_ALLOWED_HOSTS = ["https://paperless.${domain}"];
-        PAPERLESS_TRUSTED_PROXIES = ["127.0.0.1"];
+        PAPERLESS_ALLOWED_HOSTS = "paperless.${domain}";
+        PAPERLESS_CSRF_TRUSTED_ORIGINS = "https://paperless.${domain}";
+        PAPERLESS_CORS_ALLOWED_HOSTS = "https://paperless.${domain}";
+        PAPERLESS_TRUSTED_PROXIES = "127.0.0.1";
         PAPERLESS_ALLAUTH_TRUSTED_PROXY_COUNT = 1;
         PAPERLESS_PROXY_SSL_HEADER = ["HTTP_X_FORWARDED_PROTO" "https"];
         PAPERLESS_USE_X_FORWARD_HOST = false;
